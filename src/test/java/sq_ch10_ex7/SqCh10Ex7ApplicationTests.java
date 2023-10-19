ef908885067225dc97f75f5563984fe68c478629
osparking <jbpark03@gmail.com>
2023-10-19 09:12:37 +0900

    initial with 2 dependeicies

---
+++ src/test/java/sq_ch10_ex7/SqCh10Ex7ApplicationTests.java
@@ -0,0 +1,13 @@
+package sq_ch10_ex7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh10Ex7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
