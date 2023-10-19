f53679105a05f753abbcaa4c3ba4708ef22e3dcc
osparking <jbpark03@gmail.com>
2023-10-19 09:48:09 +0900

    MyUtil 사용으로 지불금액 액수 콘솔에 로깅

--- src/main/java/sq_ch10_ex7/controller/PaymentController.java
+++ src/main/java/sq_ch10_ex7/controller/PaymentController.java
@@ -1,18 +1,25 @@
 package sq_ch10_ex7.controller;
 
+import java.util.logging.Logger;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import space.jbpark.utility.MyUtil;
 import sq_ch10_ex7.dto.PaymentDetails;
 
 @RestController
 public class PaymentController {
 	
+	private final Logger logger = 
+			MyUtil.getLogger(PaymentController.class.getName());
+	
 	@PostMapping("/payment")
 	public ResponseEntity<PaymentDetails> processPayment(
 			@RequestBody PaymentDetails details) {
+		logger.info("지불금액: " + details.getAmount());
 		return ResponseEntity.accepted().body(details);
 	}
 }
